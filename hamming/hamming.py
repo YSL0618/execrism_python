671f2477d50b3f1f32b5d643721a00e884ccce71
staff <staff@web-dev.synapse.ne.jp>
2021-02-06 15:32:09 +0900

    Hamming

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,10 @@
 def distance(strand_a, strand_b):
-    pass
+
+    if len(strand_a) == len(strand_b):
+        counter = 0
+        for unit_a, unit_b in zip(list(strand_a), list(strand_b)):
+            if unit_a != unit_b:
+                counter += 1
+        return counter
+    else:
+        raise ValueError("ValueError")
