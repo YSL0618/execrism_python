ad413972a466d740e3820046e6e6675480c30dc9
staff <staff@web-dev.synapse.ne.jp>
2021-02-06 15:44:34 +0900

    Isogram

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,7 @@
 def is_isogram(string):
-    pass
+    smalls = list("abcdefghijklmnopqrsetvwxyz")
+    ans = True
+    for small in smalls:
+        if string.lower().count(small) > 1:
+            ans = False
+    return ans
