75ce7d181a71dea6b84fa116aac7977180ae7aac
Yuki Sugihara <ken.kiva@gmail.com>
2021-02-12 15:57:19 +0900

    Twelve Days

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -27,27 +27,10 @@ On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, 
 
 On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'''
 
-    lyrics = lyrics.replace('\n\n', '\n').rstrip()
-    lyrics_2 = re.sub('\:.+', '\: ', lyrics)
-    lyricslist = lyrics.splitlines()
-    lyricslist_2 = lyrics_2.splitlines()
+    lyricslist = lyrics.replace('\n\n', '\n').splitlines()
     if start_verse == end_verse:
         answer = lyricslist[start_verse -
                             1].replace('\: ', '\: \n').replace('\, ', '\, \n').splitlines()
     else:
-        answer = [lyricslist_2[n] for n in range(start_verse-1, end_verse - 1)]
+        answer = [lyricslist[n] for n in range(start_verse - 1, end_verse)]
     return answer
-
-
-print("a:")
-print([recite(n, n)[0] for n in range(1, 4)])
-print("b:")
-print([recite(n, n)[0] for n in range(4, 7)])
-print("c:")
-print([recite(n, n)[0] for n in range(1, 13)])
-print("a-2:")
-print([recite(n, n) for n in range(1, 4)])
-print("b-2:")
-print([recite(n, n) for n in range(4, 7)])
-print("c-2:")
-print([recite(n, n) for n in range(1, 13)])
